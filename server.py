f08e16263e57bbc7225ff3498c8a6d75005a4d17
Gabriel <196485544+gabriel-berthel@users.noreply.github.com>
2025-04-07 15:27:09 -0400

    Fix args

--- server.py
+++ server.py
@@ -4,9 +4,7 @@ import discord_bot
 import utils
 import argparse
 from dotenv import load_dotenv
-import os
 import sys
-
 import logging
 from sentence_transformers import SentenceTransformer
 
@@ -20,31 +18,23 @@ if os.name != "nt":
     import uvloop
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-def parse_arguments():
-    """Parse arguments to ensure .env and .yaml files are provided"""
-
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Load .env and YAML configuration files for an agent.")
-    parser.add_argument("agent", type=str, help="Path to the agent config folder")
-
+    parser.add_argument("env", type=str, help="Path to the agent config folder")
+    parser.add_argument("config", type=str, help="Path to the agent config folder")
+    
     args = parser.parse_args()
 
-    # Check if .env file exists
-    agent_conf = os.path.join(args.agent, 'agent.yaml')
-    agent_env = os.path.join(args.agent, 'agent.env')
-
-    if not os.path.isfile(agent_env):
-        print(f"Error: agent.env not found in '{args.agent}'", file=sys.stderr)
+    if not os.path.isfile(args.env):
+        print(f"Error: agent .env not found in '{args.env}'", file=sys.stderr)
         sys.exit(1)
     
-    # Check if .yaml file exists
-    if not os.path.isfile(agent_conf):
-        print(f"Error: agent.conf not found in '{args.agent}'", file=sys.stderr)
+    if not os.path.isfile(args.config):
+        print(f"Error: agent .yaml not found in '{args.config}'", file=sys.stderr)
         sys.exit(1)
 
-    return args
+    load_dotenv(args.env)
 
-if __name__ == "__main__":
-    args = parse_arguments()
-    load_dotenv(os.path.join(args.agent, 'agent.env'))
-    agent_conf = utils.load_yaml(os.path.join(args.agent, 'agent.yaml'))
+    agent_conf = utils.load_yaml(args.config)
+
     discord_bot.run(agent_conf)
